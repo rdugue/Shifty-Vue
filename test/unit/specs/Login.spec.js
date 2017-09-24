e167bdc9aa9b80543cfe4101985764d5d5f7ec9f
Ralph Dugue <rdugue1@gmail.com>
2017-09-24 06:59:40 -0400

    Adding some tests for login vue

--- test/unit/specs/Login.spec.js
+++ test/unit/specs/Login.spec.js
@@ -2,9 +2,25 @@ import Vue from 'vue'
 import Login from '@/components/Login'
 
 describe('Login.vue', () => {
+  const Constructor = Vue.extend(Login)
+  const vm = new Constructor().$mount()
+
   it('should return true for isNotFilled when form is blank', () => {
-    const Constructor = Vue.extend(Login)
-    const vm = new Constructor().$mount()
     expect(vm.$data.isNotFilled).to.equal(true)
+  })
+
+  it('should return true for isNotFilled when form is partially filled', () => {
+    vm.$data.form.company = 'AbleSoft'
+    expect(vm.$data.isNotFilled).to.equal(true)
+  })
+
+  it('should return false for isNotFilled when form is filled', () => {
+    const form = {
+      company: 'AbleSoft',
+      userId: 'ralph',
+      password: 'a1b2c3d4'
+    }
+    vm.$data.form = form
+    expect(vm.$data.isNotFilled).to.equal(false)
   })
 })
