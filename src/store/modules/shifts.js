86e9f241231d5161e9199c22128cfc0b355f7468
Ralph Dugue <rdugue1@gmail.com>
2017-10-02 05:54:01 -0400

    Fixed csv parsing error

--- src/store/modules/shifts.js
+++ src/store/modules/shifts.js
@@ -88,7 +88,6 @@ const mutations = {
     state.shifts.push(shift)
   },
   [types.UPLOAD_SHIFTS] (state, { uploads }) {
-    console.log(uploads)
     uploads.forEach(shift => { state.shifts.push(shift) })
   }
 }
